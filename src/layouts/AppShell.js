f97b3994ca22a3899ad15df8c6b3ba6b7c318e94
sewest <seanswest30@gmail.com>
2023-05-20 16:05:04 -0500

    Remove main contents

--- src/layouts/AppShell.js
+++ src/layouts/AppShell.js
@@ -1,14 +1,10 @@
 import styles from "./app-shell.module.css";
 
-export default function AppShell() {
+export default function AppShell({ children }) {
   return (
     <div className={styles.container}>
       <nav className={styles.nav}>Nav</nav>
-      <main className={styles.main}>
-        <section className={styles.buttons}>Buttons</section>
-        <section className={styles.planet}>Planet</section>
-        <section className={styles.facts}>Text</section>
-      </main>
+      <main className={styles.main}>{children}</main>
       <footer className={styles.footer}>Footer</footer>
     </div>
   );

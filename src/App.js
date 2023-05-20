134e87bc73e2f0cd0f3ed852c2e9592b1411b196
sewest <50740028+sewest@users.noreply.github.com>
2023-05-20 16:13:02 -0500

    Revert "Feature/app shell"

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
-import AppShell from "./layouts/AppShell";
-import "./App.css";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-  return <AppShell></AppShell>;
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;

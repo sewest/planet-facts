2be976fc028a25e3feae302b00921542a8082e43
sewest <seanswest30@gmail.com>
2023-05-21 14:12:29 -0500

    Add infinite zoom star background

--- src/layouts/AppShell.js
+++ src/layouts/AppShell.js
@@ -4,6 +4,7 @@ import styles from "./app-shell.module.css";
 export default function AppShell({ children }) {
   return (
     <div className={styles.container}>
+      <div className={styles.stars} />
       <Nav className={styles.Nav} />
       <main className={styles.main}>{children}</main>
       <footer className={styles.footer}>Footer</footer>
